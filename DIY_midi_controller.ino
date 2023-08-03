048e76a91b7ff329b94a349c430d452c20867116
Divesh Jadhwani <113240252+d-hackmt@users.noreply.github.com>
2023-08-03 19:19:55 +0530

    Update DIY_midi_controller.ino

--- DIY_midi_controller.ino
+++ DIY_midi_controller.ino
@@ -1,19 +1,3 @@
-/*
-  Made by Gustavo Silveira, 2022.
-  - This Sketch reads the Arduino's digital and analog ports and send midi notes and midi control change
-
-  http://www.musiconerd.com
-  http://www.youtube.com/musiconerd
-  http://facebook.com/musiconerdmusiconerd
-  http://instagram.com/musiconerd/
-  http://www.gustavosilveira.net
-  gustavosilveira@musiconerd.com
-
-  If you are using for anything that's not for personal use don't forget to give credit.
-
-  PS: Just change the value that has a comment like " //* "
-
-*/
 
 /////////////////////////////////////////////
 // Choosing your board
